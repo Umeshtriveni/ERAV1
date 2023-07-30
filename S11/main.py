e731f533389e14b6e1390d9a16411f15e52839b5
Umesh Birajdar <42119280+Umeshtriveni@users.noreply.github.com>
2023-07-30 22:51:20 +0530

    Update main.py

--- S11/main.py
+++ S11/main.py
@@ -1,65 +1,186 @@
+# Import all the required modules
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+import math
+from collections import OrderedDict
+import sys
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
-from model import Net
-from data import get_data_loaders
-from train import train
-from test import test
-import matplotlib.pyplot as plt
+from torchvision import datasets, transforms
 
+
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+
+from torch_lr_finder import LRFinder
+
+from pytorch_grad_cam import GradCAM
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+from pytorch_grad_cam.utils.image import show_cam_on_image
+
+# Install Albumentation library
+%pip install albumentations
+
+# Install LR finder
+%pip install torch-lr-finder
+
+train = datasets.CIFAR10('./data', train=True, download=True, transform=transforms.ToTensor())
+test = datasets.CIFAR10('./data', train=False, download=True, transform=transforms.ToTensor())
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck')
+
+from datasets import AlbumDataset
+from augmentations import train_set_transforms, test_set_transforms
+
+# Get the dictionary with augmentations
+train_transforms = A.Compose(train_set_transforms.values())
+test_transforms = A.Compose(test_set_transforms.values())
+
+# Create custom Dataset to support albumentations library
+train_set = AlbumDataset('./data', train=True, download=True, transform=train_transforms)
+test_set = AlbumDataset('./data', train=False, download=True, transform=test_transforms)
+
+from visualize import visualize_cifar_augmentation
+
+aug_train = AlbumDataset('./data', train=True, download=True)
+
+aug_set_transforms = {
+    'randomcrop': A.RandomCrop(height=32, width=32, p=1),
+    'horizontalflip': A.HorizontalFlip(p=1),
+    'cutout': A.CoarseDropout(max_holes=1, max_height=16, max_width=16, min_holes=1, min_height=1, min_width=1, fill_value=(0.49139968*255, 0.48215827*255, 0.44653124*255), mask_fill_value=None, p=1),
+    'normalize': A.Normalize((0.49139968, 0.48215827 ,0.44653124), (0.24703233, 0.24348505, 0.26158768)),
+    'standardize': ToTensorV2(),
+}
+
+visualize_cifar_augmentation(aug_train, aug_set_transforms)
+
+
+
+SEED = 8
+BATCH_SIZE = 512
+
+# CUDA?
+cuda = torch.cuda.is_available()
+print("CUDA Available?", cuda)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Model configuration
-dropout_value = 0.1
-model = Net().to(device)
-optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+# For reproducibility
+torch.manual_seed(SEED)
 
-# Data loaders configuration
-train_loader, test_loader = get_data_loaders(train_batch_size=128, test_batch_size=128)
+if cuda:
+    torch.cuda.manual_seed(SEED)
 
-# Training configuration
-EPOCHS = 15
+# dataloader arguments - something you'll fetch these from cmdprmt
+dataloader_args = dict(shuffle=True, batch_size=BATCH_SIZE, num_workers=0, pin_memory=True) if cuda else dict(shuffle=True, batch_size=64)
+
+# train dataloader
+train_loader = torch.utils.data.DataLoader(train_set, **dataloader_args)
+
+# test dataloader
+test_loader = torch.utils.data.DataLoader(test_set, **dataloader_args)
+
+from model import ResNet18
+from utils import get_summary
+
+get_summary(ResNet18(), (3, 32, 32))
+
+
+
+# Put the model on selected device
+model = ResNet18().to(device)
+
+# Optimization algorithm to update the weights
+LEARNING_RATE = 0.03
+WEIGHT_DECAY = 1e-4
+optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=0.9, weight_decay=WEIGHT_DECAY)
+
+# Loss Function
+criterion = nn.CrossEntropyLoss()
+
+
+# LR finder
+from torch_lr_finder import LRFinder
+lr_finder2 = LRFinder(model, optimizer, criterion, device='cuda')
+lr_finder2.range_test(train_loader, end_lr=10, num_iter=200, step_mode="exp")
+lr_finder2.plot()
+lr_finder2.reset()
+
+EPOCHS = 20
+
+MAX_LR = 8.58E-02    # Suggested LR
+STEPS_PER_EPOCH = len(train_loader)
+from training_utils import train, test, get_lr
+
+# Data to plot accuracy and loss graphs
 train_losses = []
 test_losses = []
 train_acc = []
 test_acc = []
+learning_rates = []
+test_incorrect_pred = {'images': [], 'ground_truths': [], 'predicted_vals': []}
 
-for epoch in range(EPOCHS):
-    print("EPOCH:", epoch)
-    train(model, device, train_loader, optimizer, epoch, train_losses, train_acc)
-    test(model, device, test_loader, test_losses, test_acc)
+# Scheduler
+scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer,
+                                                max_lr=MAX_LR,
+                                                steps_per_epoch=STEPS_PER_EPOCH,
+                                                epochs=EPOCHS,
+                                                pct_start=int(0.3*EPOCHS)/EPOCHS if EPOCHS != 1 else 0.5,   # 30% of total number of Epochs
+                                                div_factor=100,
+                                                three_phase=False,
+                                                final_div_factor=100,
+                                                anneal_strategy="linear"
+                                                )
 
-print('Finished Training')
+# For each epoch
+for epoch in range(1, EPOCHS+1):
+    print(f'Epoch {epoch}')
 
-# dataiter = iter(dataloader)
-# data = next(dataiter)
+    # Train the model on training dataset and append the training loss and accuracy
+    correct, processed, train_loss = train(model, device, train_loader, optimizer, criterion, scheduler)
+    train_acc.append(100 * correct / processed)
+    train_losses.append(train_loss / len(train_loader))
+    learning_rates.append(get_lr(optimizer))
 
-dataiter = iter(test_loader)
-images, labels = next(dataiter)
+    # Test the model's performance on test dataset and append the training loss and accuracy
+    correct, test_loss = test(model, device, test_loader, criterion)
+    test_acc.append(100. * correct / len(test_loader.dataset))
+    test_losses.append(test_loss)
+    
+    
+# Print loss and accuracy
+from visualize import display_loss_and_accuracies
 
-def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show() 
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-# print images
-imshow(torchvision.utils.make_grid(images))
-print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
-# Display images
-grid_img = torchvision.utils.make_grid(images)
-plt.imshow(grid_img.permute(1, 2, 0))
-plt.axis('off')
-plt.show()
+display_loss_and_accuracies(train_losses, train_acc, test_losses, test_acc)
 
 
-# Print ground truth labels
-print('Ground Truth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+from utils import get_misclassified_data
+from visualize import display_cifar_misclassified_data
 
-# Make predictions
-outputs = model(images.to(device))
-_, predicted = torch.max(outputs, 1)
+# Denormalize the data using test mean and std deviation
+inv_normalize = transforms.Normalize(
+    mean=[-0.50/0.23, -0.50/0.23, -0.50/0.23],
+    std=[1/0.23, 1/0.23, 1/0.23]
+)
 
-# Print predicted labels
-print('Predicted: ', ' '.join('%5s' % classes[predicted[j].item()] for j in range(4)))
+# Get the misclassified data from test dataset
+misclassified_data = get_misclassified_data(model, device, test_loader)
+
+# Plot the misclassified data
+display_cifar_misclassified_data(misclassified_data, classes, inv_normalize, number_of_samples=20)
+
+
+from utils import display_gradcam_output
+
+target_layers = [model.layer4[-1]]
+# targets = [ClassifierOutputTarget(7)]
+targets = None
+
+display_gradcam_output(misclassified_data, classes, inv_normalize, model, target_layers, targets, number_of_samples=20, transparency=0.70)
+
+
+target_layers = [model.layer3[-2]]
+display_gradcam_output(misclassified_data, classes, inv_normalize, model, target_layers, targets, number_of_samples=20, transparency=0.70)
